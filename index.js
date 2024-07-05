ba366db0e830982c6201f2259e5d58f53c5d44c9
Dilan <dilanalfaro2003@gamil.com>
2024-07-05 16:49:13 -0600

    juego_adivinar

---
+++ index.js
@@ -0,0 +1,44 @@
+alert("Bienvenido al juego de hallar el número aleatorio del 1 al 100, cuentas con 10 vidas");
+        
+var estado_vida = 10;
+var numero_alea = Math.floor(Math.random() * 101);
+var reiniciar = document.getElementById("reini");
+var mostrar = document.getElementById("sugerencias");
+var presionar = document.getElementById("butto");
+
+
+reiniciar.addEventListener("click",function(){
+    numero_alea = Math.floor(Math.random() * 101);
+    estado_vida=10
+    document.getElementById("simbolo_vida").innerHTML= estado_vida 
+    mostrar.innerHTML="";
+    document.getElementById("formu").reset()
+})
+
+presionar.addEventListener("click", function() {
+    var cuadro = parseInt(document.getElementById("cuadro_numero").value);
+    if (isNaN(cuadro) || cuadro == 0 || cuadro == 101) {
+        document.getElementById("sugerencias").innerHTML="Por favor, ingrese un número válido";
+        return;
+    }
+    
+    if (cuadro > numero_alea) {
+        mostrar.innerHTML="El número debe ser menor al que ingresaste" ;
+        
+    } else if (cuadro < numero_alea) {
+        mostrar.innerHTML="El número debe ser mayor al que ingresaste";
+        
+    } else if (cuadro === numero_alea) {
+        mostrar.innerHTML="¡felicidades, has encontrado el numero!";
+        return;
+        
+    }
+    
+    if (estado_vida == 0) {
+        document.getElementById("sugerencias").innerHTML="Te has quedado sin vidas";
+        return document.getElementById("simbolo_vida").innerHTML= "0"
+    }
+
+    estado_vida= estado_vida-1;
+    document.getElementById("simbolo_vida").innerHTML= estado_vida 
+});
